cd9406c2c81845f6418d0195694eb198017d2fe7
Law Rui Xi <52446119+lawruixi@users.noreply.github.com>
2022-02-24 10:32:41 +0800

    i screwed up so here's another api url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://399b4adb.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://48d8f675.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
